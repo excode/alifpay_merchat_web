251a316e7a9b0e0436ec24ab893509981546ab37
excode <kazad79@gmail.com>
2024-08-15 18:10:17 +0800

    Introducer Label

--- src/views/secure/merchantInfo/details.vue
+++ src/views/secure/merchantInfo/details.vue
@@ -109,7 +109,7 @@ await getMerchantInfoDetails(id)
     
         <ul class="list-none p-0 m-0">
             <li className="flex align-items-center py-3 px-2 border-top-1 surface-border flex-wrap">
-                <div className="text-500 w-6 md:w-2 font-medium">Contact Person</div>
+                <div className="text-500 w-6 md:w-2 font-medium">Introducer</div>
                 <div className="text-900 w-full md:w-8 md:flex-order-0 flex-order-1">{{merchantInfo.introducer}}</div>
                 
             </li>           
